8bb8d136f5cf760c551299da649028304d68404b
shota-nagato <shota.nagato@gmail.com>
2024-03-19 08:49:36 +0900

    映画新規登録

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -2,4 +2,23 @@ class Admin::MoviesController < ApplicationController
   def index
     @movies = Movie.order(created_at: :desc)
   end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+      redirect_to admin_movies_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:name, :year, :description, :image_url, :is_showing)
+  end
 end
