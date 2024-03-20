2ba8e2b3d4b098b482093198f2e0d12f17fde191
shota-nagato <shota.nagato@gmail.com>
2024-03-20 13:32:35 +0900

    座席一覧

---
+++ app/controllers/sheets_controller.rb
@@ -0,0 +1,6 @@
+class SheetsController < ApplicationController
+
+  def index
+    @sheets = Sheet.all
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
     resources :movies
   end
   resources :movies, only: [:index]
+  resources :sheets, only: [:index]
 end
